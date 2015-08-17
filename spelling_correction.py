dbdc77902415ce591c9761532859d6beff916dd8
Carolyn <carolyn.lee@yale.edu>
2015-08-16 18:30:15 -0700

    Split 'correct_spelling()' into 'find_matches()' and 'spellcheck_file()'

--- spelling_correction.py
+++ spelling_correction.py
@@ -2,31 +2,41 @@ from edit_dist import distance
 from random import choice
 
 
-def correct_spelling():
+
+def find_matches(source_word):
+	"""Finds the best match for a source word"""
+
+	min_dist = len(source_word) * 2
+	optimal_words = []
+
+	target_file = open('dict.txt', 'r')
+	for line in target_file:
+		target_word = line.rstrip()
+
+		if distance(source_word, target_word) == min_dist:
+			# Add this word to the list
+			optimal_words.append(target_word)
+
+		if distance(source_word, target_word) < min_dist:
+			min_dist = distance(source_word, target_word)
+			# re-initialize the list, with only this word as a possible correction
+			optimal_words = [target_word]
+
+	print choice(optimal_words)
+	return choice(optimal_words)
+
+
+
+def spellcheck_file():
 	"""Corrects misspellings in the source file"""
 
 	source_file = open('misspellings.txt', 'r')
 	# iterate thru source & target words
 	for line in source_file:
 		source_word = line.rstrip()
-		min_dist = len(source_word) * 2
-		optimal_words = []
+		find_matches(source_word)
 
-		target_file = open('dict.txt', 'r')
-		for line in target_file:
-			target_word = line.rstrip()
-
-			if distance(source_word, target_word) == min_dist:
-				# Add this word to the list
-				optimal_words.append(target_word)
-
-			if distance(source_word, target_word) < min_dist:
-				min_dist = distance(source_word, target_word)
-				# re-initialize the list, with only this word as a possible correction
-				optimal_words = [target_word]
-
-		print choice(optimal_words)
 
 
 if __name__ == '__main__':
-	correct_spelling()
+	spellcheck_file()
